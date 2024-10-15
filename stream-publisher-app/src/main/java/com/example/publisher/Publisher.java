4a466d5f3a2340e0f31f96401bb8991e889a8633
kimhab11 <keomhab@gmail.com>
2024-10-15 22:32:52 +0700

    init

---
+++ stream-publisher-app/src/main/java/com/example/publisher/Publisher.java
@@ -0,0 +1,11 @@
+package com.example.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Publisher {
+    public static void main(String[] args) {
+        SpringApplication.run(Publisher.class, args);
+    }
+}
