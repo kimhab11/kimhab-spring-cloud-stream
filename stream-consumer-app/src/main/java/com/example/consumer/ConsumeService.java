5b383e22a30d8cb1b4b7a7b4aa163e1477af54d9
kimhab11 <keomhab@gmail.com>
2024-10-19 12:57:25 +0700

    stream rabbit

--- stream-consumer-app/src/main/java/com/example/consumer/ConsumeService.java
+++ stream-consumer-app/src/main/java/com/example/consumer/ConsumeService.java
@@ -1,8 +1,15 @@
 package com.example.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -10,8 +17,15 @@ import java.util.Date;
 @Service
 @Slf4j
 public class ConsumeService {
+
     @StreamListener(Sink.INPUT)
-    private void receivedMessage(String msg){
-        log.info("Received Message: {}, time: {}", msg, new Date());
+    public void receivedMessage(Message<String> message){
+        // Extract Message
+        MessageHeaders headers = message.getHeaders();
+        Object contentType = message.getHeaders().get("contentType");
+
+        // Process the message
+        log.info("Data Reviewed: {}, messageId: {}, contentType: {}", message.getPayload(), headers.getId(), contentType);
+
     }
 }
--- stream-publisher-app/src/main/java/com/example/publisher/PublisherService.java
+++ stream-publisher-app/src/main/java/com/example/publisher/PublisherService.java
@@ -1,6 +1,10 @@
 package com.example.publisher;
 
 import com.example.publisher.customBinding.KimhabBinding;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -10,8 +14,13 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
 @EnableBinding(KimhabBinding.class)
 @Service
+@Slf4j
 public class PublisherService {
     @Qualifier("kimhabChannel")
     @Autowired
@@ -20,10 +29,21 @@ public class PublisherService {
     @Autowired
     private KimhabBinding source;
 
-    public void makeMessage(@RequestParam String message){
-      //  messageChannel.send(MessageBuilder.withPayload(message).build());
-        Message<String> msg = MessageBuilder.withPayload(message).build();
-       source.kimhabSource().send(msg);
+    @SneakyThrows
+    public void makeMessage(@RequestParam String message) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> messageMap = new HashMap<>();
+        messageMap.put("data", message);
+        messageMap.put("time", LocalTime.now().toString());
+        messageMap.put("app", "publisher-app");
+        String json = objectMapper.writeValueAsString(messageMap);
+        log.info("json: {}", json);
+
+
+        //  messageChannel.send(MessageBuilder.withPayload(message).build());
+        Message<String> msg = MessageBuilder.withPayload(json).build();
+        source.kimhabSource().send(msg);
         System.out.println("Sent message: " + msg);
     }
 }
